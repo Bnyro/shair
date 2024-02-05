d70d9eab20273ebffe9860d73c60eff6df0d5e10
Bnyro <bnyro@tutanota.com>
2024-02-05 18:26:11 +0100

    fix: wrong base urls when behind a proxy

--- util/strings.go
+++ util/strings.go
@@ -35,12 +35,23 @@ func GenerateSecureToken(length int) string {
 	return hex.EncodeToString(b)
 }
 
+func getHost(c echo.Context) string {
+	// if behind a proxy, it is necessary to set the "X-Forwarded-Host" header to run properly
+	forwardedFor := c.Request().Header.Get("X-Forwarded-Host")
+
+	if !IsBlank(forwardedFor) {
+		return forwardedFor
+	}
+
+	return c.Request().Host
+}
+
 func GetBaseUrl(c echo.Context) string {
 	scheme := c.Request().URL.Scheme
 	if IsBlank(scheme) {
 		scheme = "http"
 	}
-	return scheme + "://" + c.Request().Host
+	return scheme + "://" + getHost(c)
 }
 
 func MdToHTML(md string) string {
